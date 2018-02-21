bb094d6bcfa5aa4d7e5013f609376a075a1b2eab
Alexandre Mclean <alexandre.mclean@gmail.com>
2018-02-21 12:15:13 -0500

    Add more log outputs for kubernetes script

--- scripts/kubernetes.ps1
+++ scripts/kubernetes.ps1
@@ -17,11 +17,11 @@ cmd /c "$tmpBase\7z1604-x64.exe /S /qn"
 Remove-Item -Recurse -Force $tmpBase\7z1604-x64.exe
 
 $k8sUrl = "https://dl.k8s.io/v$K8S_VERSION/kubernetes-node-windows-amd64.tar.gz"
-Write-Output "Installing Kubernetes binaries..."
+Write-Output "Downloading the Kubernetes binaries..."
 Invoke-WebRequest $k8sUrl -OutFile $K8S_PATH\kubernetes-node-windows-amd64.tar.gz
 cd $K8S_PATH
 
-# doesn't work in packer??
+Write-Output "Extracting the Kubernetes binaries..."
 Start-Process -FilePath "$7zExe" -Wait -ArgumentList "e kubernetes-node-windows-amd64.tar.gz"
 Start-Process -FilePath "$7zExe" -Wait -ArgumentList "x kubernetes-node-windows-amd64.tar"
 mv kubernetes\node\bin\*.exe .
