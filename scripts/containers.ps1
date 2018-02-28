0c2593763f32ed35a16bc8bde626056a4c4b20f8
Alexandre Mclean <alexandre.mclean@gmail.com>
2018-02-28 09:36:52 -0500

    Enable Windows containers feature

--- scripts/containers.ps1
+++ scripts/containers.ps1
@@ -2,14 +2,17 @@
 # Configure Windows server for containers
 #
 
-# Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-# Install-Package -Name Docker -ProviderName DockerMsftProvider
+Write-Output "Installing Windows Containers feature..."
+Install-WindowsFeature -Name Containers
 
 Write-Output "Installing powershell NuGet provider..."
 Install-PackageProvider -Name NuGet -Force
 
 Write-Output "Installing Docker EE preview..."
-#Install-Module -Name DockerProvider -Repository PSGallery -Force
+# Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+# Install-Package -Name Docker -ProviderName DockerMsftProvider
+
+# Install-Module -Name DockerProvider -Repository PSGallery -Force
+# Install-Package -Name docker -ProviderName DockerProvider -Force
 Install-Module -Name DockerProvider -Force
-#Install-Package -Name docker -ProviderName DockerProvider -Force
 Install-Package -Name docker -ProviderName DockerProvider -RequiredVersion Preview -Force
